a4de8a6e5011a76a9d9372b7a0ac1a8fdaf71e13
Shinas Shaji <shinasshaji12@outlook.com>
2024-05-23 15:02:01 +0200

    Add gazebo package

---
+++ freddy_gazebo/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'freddy_gazebo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/load_world_and_robot.launch.py']),
+        ('share/' + package_name + '/worlds', ['worlds/my_world.sdf']),
+        ('share/' + package_name + '/urdf', ['urdf/freddy.urdf']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='shinas',
+    maintainer_email='shinasshaji12@outlook.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
