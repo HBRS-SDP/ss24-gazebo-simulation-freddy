da90e99ace6310c15b91b1089dd6da9b4f3d49f5
Shinas Shaji <shinasshaji12@outlook.com>
2024-07-30 15:25:23 +0200

    Add basic control command publishers

--- src/freddy_gazebo/setup.py
+++ src/freddy_gazebo/setup.py
@@ -26,6 +26,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            f'control_publisher = {package_name}.control_publisher:main',
         ],
     },
 )
